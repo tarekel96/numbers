0df4655c27394692fd8d099ad1e6e155f238353d
Tarek El-Hajjaoui <tarekel96@gmail.com>
2019-11-20 18:27:51 -0800

    fixed the syntax error in the if statement so the program operates correctly now

--- numbers.sh
+++ numbers.sh
@@ -6,7 +6,7 @@ echo "Type a positive integer: "
 counter=0
 read INTEGER
 while [ $counter -le "$INTEGER" ]; do
-	if  [ $(( counter % 2 == 1 )) ]; then
+	if  [ $(( counter % 2 )) == 1  ]; then
 		echo $counter "Odd"
 		counter=$((counter + 1))  
 	else	
